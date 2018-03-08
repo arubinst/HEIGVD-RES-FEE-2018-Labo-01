556a27b0671685391845d58b23cf8b1797d07bef
Elie Kin <elie.ndjoli@outlook.com>
2018-03-08 10:51:46 +0100

    FilTransformerTest OK

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -14,30 +14,35 @@ import java.io.FileFilter;
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
-    StringBuilder chaine = new StringBuilder();
+    private StringBuilder chaine = new StringBuilder();
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
     //throw new UnsupportedOperationException("The student has not implemented this method yet.");
 
-
-    //chaine.insert(0, ".\workspace\quotes" + "\n");
-
-      if(chaine.length() == 0){
+      if (chaine.length() == 0) {
           chaine.append(rootDirectory + "\n");
           vistor.visit(rootDirectory);
       }
 
-      for (File fileEntry : rootDirectory.listFiles()) {
-          if (fileEntry.isDirectory()) {
-              chaine.append(fileEntry.getPath() + "\n");
-              vistor.visit(fileEntry);
-              for(File file : fileEntry.listFiles()){
-                  if(!file.isDirectory()){
-                      chaine.append(file.getPath() + "\n");
-                      vistor.visit(file);
+    //chaine.insert(0, ".\workspace\quotes" + "\n");
+      if(rootDirectory.isDirectory()) {
+          if (chaine.length() == 0) {
+              chaine.append(rootDirectory + "\n");
+              vistor.visit(rootDirectory);
+          }
+
+          for (File fileEntry : rootDirectory.listFiles()) {
+              if (fileEntry.isDirectory()) {
+                  chaine.append(fileEntry.getPath() + "\n");
+                  vistor.visit(fileEntry);
+                  for (File file : fileEntry.listFiles()) {
+                      if (!file.isDirectory()) {
+                          chaine.append(file.getPath() + "\n");
+                          vistor.visit(file);
+                      }
                   }
+                  explore(fileEntry, vistor);
               }
-              explore(fileEntry, vistor);
           }
       }
   }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,35 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
+
+
   public UpperCaseFilterWriter(Writer wrappedWriter) {
     super(wrappedWriter);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      str = str.substring(off, off + len);
+      out.write(str.toUpperCase());
+
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    char[] chaine = new char[len];
+    for(int i = 0; i < len; i++){
+      chaine[i] = Character.toUpperCase(cbuf[i + off]);
+    }
+
+    out.write(chaine);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    out.write(Character.toUpperCase((char)c));
   }
 
 }
