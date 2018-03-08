2bf3bc9241ba12807bcb4e2dd0adf780243a433a
ansermoz <yayouzer@gmail.com>
2018-03-08 16:59:09 +0100

    First commit : Email, storeQuote, printFilename GetNextLine UpperCase

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -13,14 +13,26 @@ public class Utils {
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
    * the next line in the string passed in arguments. 
-   * 
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //if(currentChar == \r or currentChar == \n)
+    //return lines.split("\n");
+    //find + substring
+    int pos = lines.indexOf("\n");
+    int pos1 = lines.indexOf("\r");
+    if(pos > pos1){
+      pos = pos1;
+    }
+    String[] separated = new String[2];
+    separated[0] = lines.substring(0, pos);
+    separated[1] = lines.substring(pos);
+    return separated;
   }
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,31 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        out.write(str.substring(off, len + off).toUpperCase());
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        for(int i = 0; i < len; i++){
+            cbuf[i] = Character.toUpperCase(cbuf[i]);
+        }
+        out.write(cbuf, off, len);
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        c = Character.toUpperCase(c);
+        out.write(c);
+    }
 
 }
